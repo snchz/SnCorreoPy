fb0474f7fec0a7d8519ad40612a0f136953d19ca
snchz <ssaanncchheezz@gmail.com>
2019-05-07 02:23:49 +0200

    Funcional

--- sncorreopy/__init__.py
+++ sncorreopy/__init__.py
@@ -1,8 +1,10 @@
 import smtplib, ssl, imaplib
+from email.mime.text import MIMEText
 
-__version__='1.0.2-dev'
+__version__='1.0.2.dev0'
 __author__='Snchz'
 __license__='BDD'
+__doc__='Doc'
 
 
 class SNCorreo:
@@ -12,7 +14,7 @@ class SNCorreo:
 		self.servidorOUT.login(usuario,password)
 		
 		contextoSSL2=ssl.create_default_context()
-		self.servidorIN=imaplib.IMAP4_SSL(direccionServidorIMAP,puertoIMAP,context=contextoSSL2)
+		self.servidorIN=imaplib.IMAP4_SSL(direccionServidorIMAP,puertoIMAP)#,context=contextoSSL2)
 		self.servidorIN.login(usuario,password)
 		self.servidorIN.select('INBOX')
 	def crearMensaje(self,emisor, destinatario, asunto, textoMensaje):
@@ -25,7 +27,7 @@ class SNCorreo:
 		self.servidorOUT.sendmail(mensajeMIME['from'],mensajeMIME['to'],mensajeMIME.as_string())
 	def leerCorreos(self):
 		resultado, datos=self.servidorIN.search(None,'UNSEEN')
-		print datos #TODO: Hay que ojear el contenido
+		print(datos) #TODO: Hay que ojear el contenido
 		
 	def cerrarSesion(self):
 		self.servidorOUT.quit()
